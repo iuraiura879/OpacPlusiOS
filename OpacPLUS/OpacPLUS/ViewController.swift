912acaa0e3b5f0c6c7855e6c9df8c1d5fbee2cf8
iuraiura879 <iuraiura879@gmail.com>
2016-03-18 14:35:34 +0200

    master

---
+++ OpacPLUS/OpacPLUS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpacPLUS
+//
+//  Created by Iura Gutu on 18/03/16.
+//  Copyright © 2016 deCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
